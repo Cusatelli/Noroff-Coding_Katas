ea9ac043678088b2017715e5cad8a5f6b311ca78
Cusatelli <einarvandevelde@gmail.com>
2022-01-18 10:47:43 +0100

    Used built in solution in JS to fix bugs with Length of Nested Array.

--- src/Length of a Nested Array/lengthOfNestedArray.js
+++ src/Length of a Nested Array/lengthOfNestedArray.js
@@ -1,28 +1,17 @@
-let elementsInNest = 0;
+let elementsInArray = 0;
 
-function getLenght(array) {
-    elementsInNest = 0; // Reset global value on each run
-    console.log(calculateLenght(array));
+function getLength(array) {
+    elementsInArray = 0; // Reset global value on each run
+    console.log(calculateLength(array));
 }
 
-function calculateLenght(array) {
-    if(array.length <= 0) { return 0 }; // Exit and return 0 if array empty
-
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        elementsInNest += index;
-        if(element.length <= 1) {
-            index++; // Increment if no nested elements
-        } else if(element.length > 1) {
-            return calculateLenght(element);
-        }
-    }
-    return elementsInNest + 1; // Add one to count 0
+function calculateLength(array, depth = 1) {
+    return array.flat(Infinity).length;
 }
 
 console.log("%c\nLength of a Nested Array", "color:green; font-size: 1rem");
-getLenght([1, [2, 3]]); // 3
-getLenght([1, [2, [3, 4]]]); // 4
-getLenght([1, [2, [3, [4, [5, 6]]]]]); // 6
-getLenght([1, [2], 1, [2], 1]); // 5
-getLenght([]); // 0
+getLength([1, [2, 3]]); // 3
+getLength([1, [2, [3, 4]]]); // 4
+getLength([1, [2, [3, [4, [5, 6]]]]]); // 6
+getLength([1, [2], 1, [2], 1]); // 5
+getLength([]); // 0
