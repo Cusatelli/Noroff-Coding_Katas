e205789926536c976eaec5a08bc7a24dc46c55df
Cusatelli <einarvandevelde@gmail.com>
2022-01-19 10:41:26 +0100

    fix: lengthOfNestedArray to my own solution.

--- src/Length of a Nested Array/lengthOfNestedArray.js
+++ src/Length of a Nested Array/lengthOfNestedArray.js
@@ -1,13 +1,34 @@
 
+// 'One-Liner' solution
+// function getLength(array) {
+//     const length = array.flat(Infinity).length;
+//     console.log(length);
+//     return length;
+// }
+
 function getLength(array) {
-    const length = array.flat(Infinity).length;
+    const length = getDepth(array, Infinity).length;
     console.log(length);
     return length;
 }
 
+function getDepth(array, depth = 1) {
+    if(depth > 0) {
+        return array.reduce((accumulator, value) => accumulator.concat(myReducer(value, depth - 1)), []);
+    }
+    return array.slice();
+}
+
+function myReducer(value, depth) {
+    if(Array.isArray(value)) {
+        return getDepth(value, depth);
+    }
+    return value;
+}
+
 console.log("%c\nLength of a Nested Array", "color:green; font-size: 1rem");
-getLength([1, [2, 3]]); // 3
-getLength([1, [2, [3, 4]]]); // 4
-getLength([1, [2, [3, [4, [5, 6]]]]]); // 6
-getLength([1, [2], 1, [2], 1]); // 5
-getLength([]); // 0
+getLength([1, [2, 3]]);                 // 3
+getLength([1, [2, [3, 4]]]);            // 4
+getLength([1, [2, [3, [4, [5, 6]]]]]);  // 6
+getLength([1, [2], 1, [2], 1]);         // 5
+getLength([]);                          // 0
